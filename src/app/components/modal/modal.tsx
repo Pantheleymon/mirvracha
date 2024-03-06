0207ca58154bcf3190c452ec87ee15bcda8e3a78
Pantheleymon <pantheleymonbbx@yandex.ru>
2024-03-06 16:56:54 +0300

    feat: modal window

---
+++ src/app/components/modal/modal.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React, { FunctionComponent } from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+  Wrapper,
+  StyledModal,
+  Content,
+  Backdrop,
+} from './modal.style';
+
+export interface ModalProps {
+  isShown: boolean;
+  hide: () => void;
+  modalContent: JSX.Element;
+  headerText: string;
+}
+
+export const Modal: FunctionComponent<ModalProps> = ({
+  isShown,
+  hide,
+  modalContent,
+}) => {
+  const modal = (
+    <React.Fragment>
+      <Backdrop onClick={hide} />
+      <Wrapper>
+        <StyledModal>
+          <Content>{modalContent}</Content>
+        </StyledModal>
+      </Wrapper>
+    </React.Fragment>
+  );
+
+  return isShown ? ReactDOM.createPortal(modal, document.body) : null;
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+'use client'
 
-export default function Home() {
+import React, { FunctionComponent } from 'react';
+
+import { Modal } from './components/modal/modal';
+import useModal from './components/modal/useModal';
+
+const App: FunctionComponent = () => {
+  const { isShown, toggle } = useModal();
+
+  const content = <React.Fragment>Hey, I'm a model.</React.Fragment>;
+
   return (
-    <>
-    <h1>Hello world!</h1>
-    <h2>sex</h2>
-    </>
-  )
-}
+    <React.Fragment>
+        
+      <Modal isShown={isShown} hide={toggle} modalContent={content} headerText="Modal"/>
+    </React.Fragment>
+  );
+};
+
+export default App;
